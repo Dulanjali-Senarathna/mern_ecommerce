58f197ba59e928f7ec6a30962ddfca10ee15c7be
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-11-13 19:34:35 +0530

    Api Request Validation Done

--- mern_back_end/src/routes/admin/auth.js
+++ mern_back_end/src/routes/admin/auth.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const { signup, signin, requireSignin } = require('../../controller/admin/auth');
 const router = express.Router();
+const {validateSignupRequest, isRequestValidated,validateSigninRequest, } = require('../../validators/auth');
 
 
-router.post('/admin/signin', signin);
+router.post('/admin/signin',validateSigninRequest, isRequestValidated, signin);
 
-router.post('/admin/signup',signup);
-
-
+router.post('/admin/signup',validateSignupRequest, isRequestValidated,signup);
 
 module.exports = router;
